d4c34bef39b098fb59142f86e2ea88b29bf6612c
andulazizbek001 <olimjonovabdulazizbek001@gmail.com>
2024-05-07 23:24:43 +0500

    next

---
+++ src/js/header.js
@@ -0,0 +1,35 @@
+const body = document.body;
+const elHeaderLoginBox = document.querySelector('.header_login-box');
+const elHeaderRezult = document.querySelector('.rezult');
+
+elHeaderLoginBox.style.display = 'none';
+elHeaderRezult.style.display = 'none';
+
+
+function HeaderLogin() {
+    body.style.cssText = `
+        overflow: hidden;
+    `
+
+    elHeaderLoginBox.style.cssText = `
+        display: inline-block;
+    `
+
+    elHeaderRezult.style.cssText = `
+        display: inline-block;
+    `
+}
+
+function HeaderCloseModal() {
+    body.style.cssText = `
+        flex-direction: column;
+    `
+
+    elHeaderLoginBox.style.cssText = `
+        display: none;
+    `
+
+    elHeaderRezult.style.cssText = `
+        display: none;
+    `
+}
